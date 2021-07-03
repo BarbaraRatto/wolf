4b634fc9e4d73f225a4577667dda9c9086443722
Gennaro Raiola <gennaro.raiola@gmail.com>
2021-07-03 18:38:30 +0200

    Update test id problem

--- wb_controller/test/test_id_problem.cpp
+++ wb_controller/test/test_id_problem.cpp
@@ -5,21 +5,52 @@
 
 static wb_controller::QuadrupedRobot::Ptr _robot;
 static wb_controller::IDProblem::Ptr _id_problem;
+static std::unique_ptr<ros::NodeHandle> _root_nh_ptr; // FIXME
 
 // TEST CASES
 TEST(IDProblem, Constructor)
 {
-    //_id_problem.reset(new OpenSoT::IDProblem(_robot->getRosNode(),_robot->getXBotModel(),_robot->getFootNames(),""));
+    _id_problem.reset(new wb_controller::IDProblem(*_root_nh_ptr,_robot));
 }
+
+TEST(IDProblem, Solve)
+{
+    Eigen::VectorXd x;
+    ASSERT_TRUE(_id_problem->solve(x));
+}
+
+TEST(IDProblem, SwitchStack)
+{
+    Eigen::VectorXd x;
+    ASSERT_TRUE(_id_problem->solve(x));
+    _id_problem->switchStack();
+    ASSERT_TRUE(_id_problem->solve(x));
+}
+
+TEST(IDProblem, SelectStackWalking)
+{
+    _id_problem->selectStack(wb_controller::IDProblem::stacks_t::WALKING);
+    Eigen::VectorXd x;
+    ASSERT_TRUE(_id_problem->solve(x));
+}
+
+TEST(IDProblem, SelectStackManipulation)
+{
+    _id_problem->selectStack(wb_controller::IDProblem::stacks_t::MANIPULATION);
+    Eigen::VectorXd x;
+    ASSERT_TRUE(_id_problem->solve(x));
+}
+
+
 
 int main(int argc, char** argv)
 {
 
   ros::init(argc, argv, "test_id_problem");
   testing::InitGoogleTest(&argc, argv);
-  ros::NodeHandle root_nh;
 
-  _robot.reset(wb_controller::createRobotModel(root_nh));
+  _root_nh_ptr = std::make_unique<ros::NodeHandle>();
+  _robot.reset(wb_controller::createRobotModel(*_root_nh_ptr));
 
   ros::AsyncSpinner spinner(1);
   spinner.start();
