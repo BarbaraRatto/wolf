06dc8a6e51932aeea36fb47e3d5d3f2299cc87e9
EnricoMingo <enricomingo@gmail.com>
2021-08-31 21:41:29 +0200

    switched to new joy handler class

--- wb_controller/include/wb_controller/devices/joy.h
+++ wb_controller/include/wb_controller/devices/joy.h
@@ -79,8 +79,14 @@ protected:
 
 class JoyHandler : public DeviceHandlerRosInterface<sensor_msgs::Joy::ConstPtr>
 {
+private:
+    bool XBOX_ = false;
 
 public:
+    void setXBOXController(const bool enable_xbox_controller)
+    {
+        XBOX_ = enable_xbox_controller;
+    }
 
     enum button_t {START=0,SELECT,ONE,TWO,THREE,FOUR};
 
@@ -147,42 +153,67 @@ public:
 
         if(msg.get() && !msg->axes.empty() && !msg->buttons.empty())
         {
+            if(XBOX_)
+            {
+                base_velocity_y_scale_     = static_cast<double>(msg->axes[0]);
+                base_velocity_x_scale_     = static_cast<double>(msg->axes[1]);
+                base_velocity_z_scale_     = static_cast<double>(msg->axes[7]); //R1 and R2
 
-          base_velocity_y_scale_     = static_cast<double>(msg->axes[0]);
-          base_velocity_x_scale_     = static_cast<double>(msg->axes[1]);
-          base_velocity_z_scale_     = (static_cast<double>(msg->buttons[5])-static_cast<double>(msg->buttons[7])); //R1 and R2
+                base_yaw_scale_         = static_cast<double>(msg->axes[3]);
+                base_pitch_scale_       = static_cast<double>(msg->axes[4]);
+                base_roll_scale_        = -static_cast<double>(msg->axes[6]);
 
-          base_yaw_scale_         = static_cast<double>(msg->axes[2]);
-          base_pitch_scale_       = static_cast<double>(msg->axes[3]);
-          base_roll_scale_        = -static_cast<double>(msg->axes[4]);
+                start_swing_     = static_cast<bool>(msg->buttons[4]); // L1 button
+                reset_base_      = static_cast<bool>(msg->buttons[5]); // R1 button
 
-          start_swing_     = static_cast<bool>(msg->buttons[4]); // L1 button
-          reset_base_      = static_cast<bool>(msg->buttons[6]); // L2 button
+                joy_up_down_trigger_.update(static_cast<double>(msg->axes[5]));
 
-          joy_up_down_trigger_.update(static_cast<double>(msg->axes[5]));
+                if(f_start_ && joy_start_button_trigger_.update(static_cast<bool>(msg->buttons[6])))
+                    f_start_();
 
-          if(joy_up_down_trigger_.getStatus() == wb_controller::AxisToTrigger::UP)
-              controller_ptr_->getFootholdsPlanner()->increaseStepHeight();
-          else if (joy_up_down_trigger_.getStatus() == wb_controller::AxisToTrigger::DOWN)
-              controller_ptr_->getFootholdsPlanner()->decreaseStepHeight();
+                if(f_select_ && joy_select_button_trigger_.update(static_cast<bool>(msg->buttons[7])))
+                    f_select_();
+            }
+            else
+            {
+              base_velocity_y_scale_     = static_cast<double>(msg->axes[0]);
+              base_velocity_x_scale_     = static_cast<double>(msg->axes[1]);
+              base_velocity_z_scale_     = (static_cast<double>(msg->buttons[5])-static_cast<double>(msg->buttons[7])); //R1 and R2
 
-          if(f_start_ && joy_start_button_trigger_.update(static_cast<bool>(msg->buttons[9])))
-              f_start_();
+              base_yaw_scale_         = static_cast<double>(msg->axes[2]);
+              base_pitch_scale_       = static_cast<double>(msg->axes[3]);
+              base_roll_scale_        = -static_cast<double>(msg->axes[4]);
 
-          if(f_select_ && joy_select_button_trigger_.update(static_cast<bool>(msg->buttons[8])))
-              f_select_();
+              start_swing_     = static_cast<bool>(msg->buttons[4]); // L1 button
+              reset_base_      = static_cast<bool>(msg->buttons[6]); // L2 button
 
-          if(f_one_ && joy_one_button_trigger_.update(static_cast<bool>(msg->buttons[0])))
-              f_one_();
+              joy_up_down_trigger_.update(static_cast<double>(msg->axes[5]));
 
-          if(f_two_ && joy_two_button_trigger_.update(static_cast<bool>(msg->buttons[1])))
-              f_two_();
 
-          if(f_three_ && joy_three_button_trigger_.update(static_cast<bool>(msg->buttons[2])))
-              f_three_();
+              if(f_start_ && joy_start_button_trigger_.update(static_cast<bool>(msg->buttons[9])))
+                  f_start_();
 
-          if(f_four_ && joy_four_button_trigger_.update(static_cast<bool>(msg->buttons[3])))
-              f_four_();
+              if(f_select_ && joy_select_button_trigger_.update(static_cast<bool>(msg->buttons[8])))
+                  f_select_();
+
+              if(f_one_ && joy_one_button_trigger_.update(static_cast<bool>(msg->buttons[0])))
+                  f_one_();
+
+              if(f_two_ && joy_two_button_trigger_.update(static_cast<bool>(msg->buttons[1])))
+                  f_two_();
+
+              if(f_three_ && joy_three_button_trigger_.update(static_cast<bool>(msg->buttons[2])))
+                  f_three_();
+
+              if(f_four_ && joy_four_button_trigger_.update(static_cast<bool>(msg->buttons[3])))
+                  f_four_();
+            }
+
+            if(joy_up_down_trigger_.getStatus() == wb_controller::AxisToTrigger::UP)
+                controller_ptr_->getFootholdsPlanner()->increaseStepHeight();
+            else if (joy_up_down_trigger_.getStatus() == wb_controller::AxisToTrigger::DOWN)
+                controller_ptr_->getFootholdsPlanner()->decreaseStepHeight();
+
 
           update();
 
