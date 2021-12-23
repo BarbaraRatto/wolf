948983090a712c9c85c10569e6c1c4047b60f44c
Gennaro Raiola <gennaro.raiola@gmail.com>
2021-12-23 14:31:30 +0100

    Add back waistZ

--- wb_controller/src/id_problem.cpp
+++ wb_controller/src/id_problem.cpp
@@ -150,7 +150,7 @@ IDProblem::IDProblem(ros::NodeHandle& nh, QuadrupedRobot::Ptr model, const doubl
   for(unsigned int i=1;i<foot_names_.size();i++)
     feet_aggregated = feet_aggregated + feet_[foot_names_[i]]%id_XYZ;
 
-  stack_ /= (feet_aggregated +waistRPY_%id_RPY + angular_momentum_ + com_);
+  stack_ /= (feet_aggregated + waistRPY_%id_RPY + waistZ_%id_Z + angular_momentum_ + com_);
 
   if(ee_names_.size() > 0)
   {
