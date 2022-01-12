b95e4cafe1b330ca2e29a47feda536cbef14a438
Gennaro Raiola <gennaro.raiola@gmail.com>
2022-01-12 16:29:31 +0100

    Test cartesian trajectory

--- wolf_controller/include/wolf_controller/cartesian_trajectory.h
+++ wolf_controller/include/wolf_controller/cartesian_trajectory.h
@@ -3,6 +3,7 @@
 
 #include <memory>
 #include <Eigen/Core>
+#include <vector>
 #include <cartesian_interface/trajectory/Trajectory.h>
 #include <OpenSoT/tasks/acceleration/Cartesian.h>
 
@@ -14,8 +15,13 @@ class CartesianTrajectory
 
 public:
 
-  enum state_t {ONLINE=0,REACHING};
-  enum type_t {INTERACTIVE=0,BATCH};
+  enum state_t {IDLE=0,EXECUTING};
+
+  struct WayPoint
+  {
+    Eigen::Affine3d T_ref;
+    double duration;
+  };
 
   const std::string CLASS_NAME = "CartesianTrajectory";
 
@@ -35,18 +41,22 @@ public:
 
   void reset();
 
+  double getTime() const;
+
   bool getReference(Eigen::Affine3d& T_ref,
                     Eigen::Vector6d* vel_ref = nullptr,
                     Eigen::Vector6d* acc_ref = nullptr) const;
 
   bool setWayPoint(const Eigen::Affine3d& T_ref, double duration);
 
+  bool setWayPoints(const std::vector<WayPoint>& wps);
+
+  state_t getState() const;
+
 private:
 
-  double time_;
-
-  state_t state_;
-  type_t type_;
+  std::atomic<double> time_;
+  std::atomic<state_t> state_;
 
   Eigen::Affine3d T_;
   Eigen::Vector6d vel_;
@@ -55,7 +65,7 @@ private:
   XBot::Cartesian::Trajectory::Ptr trajectory_;
   OpenSoT::tasks::acceleration::Cartesian* task_ptr_;
 
-  bool check_reach() const;
+  bool checkProximity() const;
 };
 
 
--- wolf_controller/src/cartesian_trajectory.cpp
+++ wolf_controller/src/cartesian_trajectory.cpp
@@ -11,18 +11,16 @@ const double DEFAULT_REACH_THRESHOLD = 1e-6;
 namespace wolf_controller {
 
 CartesianTrajectory::CartesianTrajectory(OpenSoT::tasks::acceleration::Cartesian* const task_ptr)
-  :type_(type_t::INTERACTIVE)
-  ,state_(state_t::ONLINE)
+  :state_(state_t::IDLE)
   ,task_ptr_(task_ptr)
 {
   trajectory_ = std::make_shared<Trajectory>();
-  time_ = 0.0;
   reset();
 }
 
 void CartesianTrajectory::reset()
 {
-  state_ = state_t::ONLINE;
+  state_ = state_t::IDLE;
   if(task_ptr_!=nullptr)
     task_ptr_->getActualPose(T_);
   else
@@ -30,63 +28,85 @@ void CartesianTrajectory::reset()
   vel_.setZero();
   acc_.setZero();
   trajectory_->clear();
+  time_ = 0.0;
 }
 
 void CartesianTrajectory::update(double period)
 {
-    switch(state_)
-    {
-        case state_t::REACHING:
-          T_ = trajectory_->evaluate(time_, &vel_, &acc_);
-          if(trajectory_->isTrajectoryEnded(time_) && check_reach())
-            state_ = state_t::ONLINE;
-        break;
+  switch(state_)
+  {
+  case state_t::EXECUTING:
+    T_ = trajectory_->evaluate(time_, &vel_, &acc_);
+    if(trajectory_->isTrajectoryEnded(time_) && checkProximity())
+      state_ = state_t::IDLE;
+    time_ = time_ + period;
+    break;
 
-        case state_t::ONLINE:
-          vel_.setZero();
-          acc_.setZero();
-        break;
-    };
-
-    time_+=period;
+  case state_t::IDLE:
+    vel_.setZero();
+    acc_.setZero();
+    time_ = 0.0;
+    break;
+  };
 }
 
 bool CartesianTrajectory::getReference(Eigen::Affine3d& T_ref,
-                                 Eigen::Vector6d* vel_ref,
-                                 Eigen::Vector6d* acc_ref) const
+                                       Eigen::Vector6d* vel_ref,
+                                       Eigen::Vector6d* acc_ref) const
 {
-    T_ref = T_;
+  T_ref = T_;
 
-    if(vel_ref) *vel_ref = vel_;
-    if(acc_ref) *acc_ref = acc_;
+  if(vel_ref) *vel_ref = vel_;
+  if(acc_ref) *acc_ref = acc_;
 
-    return true;
+  return true;
 }
 
 bool CartesianTrajectory::setWayPoint(const Eigen::Affine3d& T_ref, double duration)
 {
-  if(state_ == state_t::REACHING)
+  if(state_ == state_t::EXECUTING)
     return false;
 
-  state_ = state_t::REACHING;
+  state_ = state_t::EXECUTING;
 
-  //if(type_ == type_t::INTERACTIVE)
-  //{
   trajectory_->clear();
   trajectory_->addWayPoint(0.0, T_);
   trajectory_->addWayPoint(duration, T_ref);
   trajectory_->compute();
-  time_ = 0.0;
-  //}
+
   return true;
 }
 
-
-
-bool CartesianTrajectory::check_reach() const
+bool CartesianTrajectory::setWayPoints(const std::vector<WayPoint> &wps)
 {
-    auto T_ref = trajectory_->getWayPoints().back().frame;
-    return T_ref.isApprox(T_, DEFAULT_REACH_THRESHOLD);
+  if(state_ == state_t::EXECUTING)
+    return false;
+
+  state_ = state_t::EXECUTING;
+
+  trajectory_->clear();
+  trajectory_->addWayPoint(0.0, T_);
+  for(unsigned int i=0; i<wps.size();i++)
+    trajectory_->addWayPoint(wps[i].duration, wps[i].T_ref);
+  trajectory_->compute();
+
+  return true;
+}
+
+CartesianTrajectory::state_t CartesianTrajectory::getState() const
+{
+  return state_;
+}
+
+bool CartesianTrajectory::checkProximity() const
+{
+  auto T_ref = trajectory_->getWayPoints().back().frame;
+  return T_ref.isApprox(T_, DEFAULT_REACH_THRESHOLD);
+}
+
+double wolf_controller::CartesianTrajectory::getTime() const
+{
+  return time_;
 }
 
 }; // namespace
