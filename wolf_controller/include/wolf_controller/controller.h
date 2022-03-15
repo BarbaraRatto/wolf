b6b36938ba542659da657919ff99885dd0a371db
graiola <gennaro.raiola@gmail.com>
2022-03-15 20:56:55 +0100

    Use tf2

--- wolf_controller/include/wolf_controller/controller.h
+++ wolf_controller/include/wolf_controller/controller.h
@@ -12,7 +12,7 @@ work. If not, see <http://creativecommons.org/licenses/by-nc-nd/4.0/>.
 
 // ROS
 #include <ros/ros.h>
-#include <tf/transform_broadcaster.h>
+#include <tf2_ros/transform_broadcaster.h>
 #include <nav_msgs/Odometry.h>
 // PluginLib
 #include <pluginlib/class_list_macros.hpp>
